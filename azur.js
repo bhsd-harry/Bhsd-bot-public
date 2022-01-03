dd2a59c2fac1af1ea0bc080d054f31588413b78b
bhsd <2545473905@qq.com>
2022-01-02 21:27:44 -0800

    修复正则的错误

--- azur.js
+++ azur.js
@@ -14,9 +14,10 @@ const url = 'https://zh.moegirl.org.cn',
 	if (mode !== 'dry') {
 		await api.csrfToken();
 	}
-	const regex1 = /{{\s*(?:碧[蘭蓝]航[線线])?背景[圖图]片\s*\|(.+?)}}/,
-		regex2 = /{{\s*碧[蘭蓝]航[線线]\s*(?:\|.*?)?}}/,
-		regex3 = /\|\s*(?!position\s*=\s*(?:center|50%)\s*(?:center|50%)?\s*(?:\||}}))/,
+	const regex1 = /{{\s*(?:碧[蘭蓝]航[線线])?背景[圖图]片\s*\|(.+?)}}/, // 捕获定制的背景图片
+		regex2 = /{{\s*碧[蘭蓝]航[線线]\s*(?:\|.*?)?(?=}})/, // 匹配大家族模板（不含右半}}）
+		regex3 = /\|\s*(?!position\s*=\s*center\s*(?:\||}}))/, // 匹配有效的背景图片参数
+		regex4 = /\|\s*position\s*=\s*center\s*(?=\||}})/g, // 匹配无效的背景图片参数，与regex3互补
 		pages = await api.search('hastemplate:"碧蓝航线" insource:"背景图片"');
 	const list = pages.map(({pageid, content}) => {
 		const raw = content.match(regex1);
@@ -25,20 +26,20 @@ const url = 'https://zh.moegirl.org.cn',
 			return null;
 		}
 		if (!regex2.test(content)) {
-			error(`页面 ${pageid} 未找到大家族模板！`);
+			error(`页面 ${pageid} 未找到直接引用的大家族模板！`);
 			return null;
 		}
 		let text;
-		if (regex3.test(raw[1])) { // 额外规定了背景图片样式
+		if (regex3.test(raw[1])) { // 额外规定了背景图片的有效参数
 			const nav = content.match(regex2);
 			if (/\|\s*无背景\s*=/.test(nav[0])) { // 已修复
-				error(`页面 ${pageid} 已修复！`);
+				info(`页面 ${pageid} 已修复！`);
 				return null;
 			}
-			text = content.replace(regex2, p => `${p.slice(0, -2)}|无背景=1}}`);
-		} else { // eslint-disable-line no-else-return
-			const image = raw[1].replace(regex3, '').replace(/^(?:file:|url\s*=)\s*/i, '');
-			text = content.replace(regex1, '').replace(regex2, p => `${p.slice(0, -2)}|2=${image}}}`);
+			text = content.replace(regex2, '$&|无背景=1');
+		} else {
+			const image = raw[1].replace(regex4, '').replace(/^(?:file:|url\s*=)\s*/i, '');
+			text = content.replace(regex1, '').replace(regex2, `$&|2=${image}`);
 		}
 		return [pageid, content, text];
 	}).filter(page => page);
