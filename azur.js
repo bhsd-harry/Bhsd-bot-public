87e7234afd40cdff6994fbcd84b22c4849c096f0
bhsd <2545473905@qq.com>
2022-01-04 17:21:56 -0800

    正确匹配换行符

--- azur.js
+++ azur.js
@@ -14,32 +14,36 @@ const url = 'https://zh.moegirl.org.cn',
 	if (mode !== 'dry') {
 		await api.csrfToken();
 	}
-	const regex1 = /{{\s*(?:碧[蘭蓝]航[線线])?背景[圖图]片\s*\|(.+?)}}/, // 捕获定制的背景图片
-		regex2 = /{{\s*碧[蘭蓝]航[線线]\s*(?:\|.*?)?(?=}})/, // 匹配大家族模板（不含右半}}）
-		regex3 = /\|\s*(?!position\s*=\s*center\s*(?:\||}}))/, // 匹配有效的背景图片参数
-		regex4 = /\|\s*position\s*=\s*center\s*(?=\||}})/g, // 匹配无效的背景图片参数，与regex3互补
+	const regex1 = /{{\s*(?:碧[蘭蓝]航[線线])?背景[圖图]片\s*\|([\s\S]+?)}}/, // 捕获定制的背景图片
+		regex2 = /{{\s*(?:碧[蘭蓝]航[線线])?背景[圖图]片\s*\|?}}/g, // 错误模板用法
+		regex3 = /{{\s*碧[蘭蓝]航[線线]\s*(?:\|[\s\S]*?)?(?=}})/, // 匹配大家族模板（不含右半}}）
+		// 匹配无效的背景图片参数
+		regex4 = /\|\s*(?:position\s*=\s*center|logo-url\s*=[\s\S]*?|logo-size\s*=\s*contain)\s*(?=\||$)/g,
 		pages = await api.search('hastemplate:"碧蓝航线" insource:"背景图片"');
 	const list = pages.map(({pageid, content}) => {
-		const raw = content.match(regex1);
-		if (!raw) {
+		const template = content.match(regex1);
+		let text;
+		if (regex2.test(content)) { // 错误模板用法
+			text = content.replace(regex2, '');
+		} else if (!template) { // 未知原因
 			error(`页面 ${pageid} 未找到直接引用的背景图片模板！`);
 			return null;
-		}
-		if (!regex2.test(content)) {
+		} else if (!regex3.test(content)) { // 未出现过此种情形
 			error(`页面 ${pageid} 未找到直接引用的大家族模板！`);
 			return null;
-		}
-		let text;
-		if (regex3.test(raw[1])) { // 额外规定了背景图片的有效参数
-			const nav = content.match(regex2);
-			if (/\|\s*无背景\s*=/.test(nav[0])) { // 已修复
-				info(`页面 ${pageid} 已修复！`);
-				return null;
-			}
-			text = content.replace(regex2, '$&|无背景=1');
 		} else {
-			const image = raw[1].replace(regex4, '').replace(/^(?:file:|url\s*=)\s*/i, '');
-			text = content.replace(regex1, '').replace(regex2, `$&|2=${image}`);
+			const params = template[1].replace(regex4, '');
+			if (params.includes('|')) { // 额外规定了背景图片的有效参数
+				const nav = content.match(regex3);
+				if (/\|\s*无背景\s*=/.test(nav[0])) { // 已修复
+					info(`页面 ${pageid} 已修复！`);
+					return null;
+				}
+				text = content.replace(regex3, '$&|无背景=1');
+			} else { // 将背景图片合并至大家族模板
+				const image = params.replace(/^(?:file:|url\s*=)\s*/i, '').trim();
+				text = content.replace(regex1, '').replace(regex3, `$&|2=${image}`);
+			}
 		}
 		return [pageid, content, text];
 	}).filter(page => page);
