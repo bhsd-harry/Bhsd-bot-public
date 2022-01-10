f6141132833379532bdfbc7bad557b3bc428c1ce
bhsd <2545473905@qq.com>
2022-01-09 17:30:11 -0800

    Update cmoegirl.js

--- cmoegirl.js
+++ cmoegirl.js
@@ -2,19 +2,19 @@
  * @Function: 仅用于生成萌娘共享需要的Api对象和QQ方法
  */
 'use strict';
-const Api = require('./api.js'),
-	QQ = require('./qq.js'),
-	Rc = require('./rc.js'),
+const Api = require('../Bhsd-bot-public/api.js'),
+	QQ = require('../Bhsd-bot-public/qq.js'),
+	Rc = require('../Bhsd-bot-public/rc.js'),
 	{promises: fs} = require('fs'),
-	{error} = require('./dev.js'),
-	categories = require('./categories.json'),
-	{user, pin, account, uid, gid} = require('./user.json');
+	{error} = require('../Bhsd-bot-public/dev.js'),
+	categories = require('../Bhsd-bot-public/categories.json'),
+	{user, pin, account, uid, gid} = require('../Bhsd-bot-public/user.json');
 
 const api = new Api(user, pin, 'https://commons.moegirl.org.cn', '萌娘共享'),
 	qq = global.qq || new QQ(account, uid),
 	rc = new Rc(api, qq, gid, {rcnamespace: '6|14', grcnamespace: '6|14'}, categories);
 
-let rcstart = require('./cmoegirl.json'),
+let rcstart = require('../Bhsd-bot-public/cmoegirl.json'),
 	watchTimer, informTimer;
 
 const _notOnline = () => {
@@ -29,7 +29,7 @@ const _post = async (time) => { // 输入参数仅用来规避require-atomic-upd
 		return;
 	}
 	rcstart = await rc.post(time);
-	fs.writeFile('cmoegirl.json', JSON.stringify(rcstart));
+	fs.writeFile('../Bhsd-bot-public/cmoegirl.json', JSON.stringify(rcstart));
 	watchTimer = setTimeout(() => {
 		_post(rcstart);
 	}, 1000 * 60 * 10);
