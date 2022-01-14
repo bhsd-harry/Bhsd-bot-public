d548ad2f106ef49927db89ade7cddde375057369
bhsd <2545473905@qq.com>
2022-01-13 16:44:57 -0800

    添加新浪图片

--- extImage.js
+++ extImage.js
@@ -7,16 +7,25 @@ const Api = require('./api.js'),
 	{error} = require('./dev.js');
 
 const api = new Api(user, pin, 'https://zh.moegirl.org.cn'),
-	[,, mode] = process.argv;
+	[,, mode] = process.argv,
+	regex = /https:\/\/(?:i\d\.hdslb\.com|ww\d\.sinaimg\.cn)\/[\S]+\.(?:jpe?g|png|gif|tiff|bmp)/gi;
+
+const search = (site) => api.search(`insource:"https://${site}" -hastemplate:"Noreferer"`);
 
 (async () => {
 	await api[mode === 'dry' ? 'login' : 'csrfToken']();
-	const pages = (await Promise.all(new Array(10).fill().map((_, i) =>
-		api.search(`insource:"https://i${i}.hdslb.com" -hastemplate:"Noreferer"`)
-	))).flat();
+	if (mode === 'rerun') {
+		await api.massEdit(null, mode, '自动修复引自bilibili或新浪的图片外链');
+		return;
+	}
+	// i[0-2].hdslb.com或ww[1-4].sinaimg.cn
+	const pages = (await Promise.all([
+		...new Array(3).fill().map((_, i) => search(`i${i}.hdslb.com`)),
+		...new Array(4).fill().map((_, i) => search(`ww${i + 1}.sinaimg.cn`))
+	])).flat();
 	const pageids = [...new Set(pages.map(({pageid}) => pageid))],
 		edits = pageids.map(pageid => pages.find(({pageid: id}) => id === pageid)).map(({pageid, content}) => {
-		const urls = content.match(/https:\/\/i\d\.hdslb\.com\/[\S]+\.(?:jpe?g|png|gif|tiff|bmp)/gi);
+		const urls = content.match(regex);
 		if (!urls) {
 			error(`页面 ${pageid} 找不到图片链接！`);
 			return null;
@@ -27,5 +36,5 @@ const api = new Api(user, pin, 'https://zh.moegirl.org.cn'),
 		});
 		return [pageid, content, text];
 	}).filter(edit => edit);
-	api.massEdit(edits, mode, '自动修复引自bilibili的图片外链');
+	api.massEdit(edits, mode, '自动修复引自bilibili或新浪的图片外链');
 })();
