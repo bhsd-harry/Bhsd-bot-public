0986570190be99d50c701a52b74847999e9b887b
bhsd <2545473905@qq.com>
2022-03-24 21:14:33 -0700

    test(moegirl.js):尝试临时替代葫芦bot

--- interactive/cmoegirl.js
+++ interactive/cmoegirl.js
@@ -11,6 +11,6 @@ const Api = require('../lib/api.js'),
 const api = new Api(user, cmpin, 'https://commons.moegirl.org.cn', '萌娘共享'),
 	qq = global.qq || new QQ(account, uid, password),
 	params = {rcnamespace: '6|14', grcnamespace: '6|14'},
-	rc = new Rc(api, qq, gid, '../../Bhsd-bot-public/config/cmoegirl.json', params, categories);
+	rc = new Rc(api, qq, gid, '../config/cmoegirl.json', params, categories);
 
 module.exports = {qq, rc};
---
+++ interactive/moegirl.js
@@ -0,0 +1,16 @@
+/**
+ * @Function: 仅用于临时代替葫芦bot，生成萌娘百科需要的Rc对象
+ */
+'use strict';
+const Api = require('../lib/api.js'),
+	QQ = require('../lib/qq.js'),
+	Rc = require('../lib/rc.js'),
+	categories = require('../config/mcategories.json'),
+	{user, pin, url, account, uid, gid, password} = require('../config/user.json');
+
+const api = new Api(user, pin, url, '萌娘百科'),
+	qq = global.qq || new QQ(account, uid, password),
+	params = {rcnamespace: '0|10|14', grcnamespace: '0|10|14'},
+	rc = new Rc(api, qq, null, '../config/hulubot.json', params, categories);
+
+module.exports = {qq, rc};
