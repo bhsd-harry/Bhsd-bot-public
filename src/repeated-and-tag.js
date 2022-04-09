6a5cd554f4f8492c3d80058c7a1c30e9e0c647a5
bhsd <2545473905@qq.com>
2022-04-09 15:00:55 -0700

    fix(repeated-and-tag.js): cron邮件使用plain text

--- src/repeated-and-tag.js
+++ src/repeated-and-tag.js
@@ -1,7 +1,7 @@
 'use strict';
 const {user, pin, url} = require('../config/user.json'),
 	Api = require('../lib/api.js'),
-	{info, error, runMode} = require('../lib/dev.js'),
+	{runMode} = require('../lib/dev.js'),
 	repeated = require('./repeated.js'),
 	tag = require('./tag.js');
 
@@ -14,16 +14,16 @@ const api = new Api(user, pin, url);
 	await api.csrfToken();
 	try {
 		await repeated(api);
-		info('repeated.js已执行！');
+		console.log('repeated.js已执行！');
 	} catch (e) {
-		error('repeated.js执行过程中发生错误！');
+		console.log('repeated.js执行过程中发生错误！');
 		console.error(e);
 	}
 	try {
 		await tag(api);
-		info('tag.js已执行！');
+		console.log('tag.js已执行！');
 	} catch (e) {
-		error('tag.js执行过程中发生错误！');
+		console.log('tag.js执行过程中发生错误！');
 		console.error(e);
 	}
 })();
