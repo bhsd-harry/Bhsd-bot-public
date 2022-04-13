ba46bb0746a8c7e9280ceab23ebe521c1fd0eee1
bhsd <2545473905@qq.com>
2022-04-13 11:07:46 -0700

    feat(tag.js): 增加测试模式

--- src/tag.js
+++ src/tag.js
@@ -2,20 +2,21 @@
  * @Function: 检查[[Category:使用无效自封闭HTML标签的页面]]并修复
  */
 'use strict';
-const {user, pin, url} = require('../config/user.json'),
+const fs = require('fs'),
+	{user, pin, url} = require('../config/user.json'),
 	Api = require('../lib/api.js'),
 	{error, runMode} = require('../lib/dev.js');
 
-const main = async (api = new Api(user, pin, url)) => {
-	const mode = runMode();
-	if (!module.parent) {
-		await api[mode === 'dry' ? 'login' : 'csrfToken']();
-		if (mode === 'rerun') {
-			await api.massEdit(null, mode, '自动修复无效自封闭的HTML标签');
-			return;
-		}
+const _analyze = (content, regex) => content.replace(regex, (p0, p1) => {
+	if (p0.slice(p1.length + 1, -2)) { // 这大概率是一个独立标签
+		return `${p0.slice(0, -2)}></${p1}>`;
 	}
-	const tags = [ // 同步自CodeMirror扩展，移除了空标签和非HTML标签
+	return `</${p1}>`; // 这大概率是一个错误的闭合标签
+});
+
+const main = async (api = new Api(user, pin, url)) => {
+	const mode = runMode('test'),
+		tags = [ // 同步自CodeMirror扩展，移除了空标签和非HTML标签
 			'b', 'bdi', 'del', 'i', 'ins',
 			'u', 'font', 'big', 'small', 'sub', 'sup',
 			'h[1-6]', 'cite',
@@ -27,8 +28,21 @@ const main = async (api = new Api(user, pin, url)) => {
 			'li', 'dt', 'dd', 'td', 'th',
 			'tr',
 		],
-		regex = new RegExp(`<(${tags.join('|')})(?:\\s+[^>]*?)?/>`, 'gi'),
-		pages = await api.categorymembers('使用无效自封闭HTML标签的页面');
+		regex = new RegExp(`<(${tags.join('|')})(?:\\s+[^>]*?)?/>`, 'gi');
+	if (mode === 'test') {
+		const content = fs.readFileSync('test.txt', 'utf8'),
+			text = _analyze(content, regex);
+		await api.massEdit([[0, content, text]], 'dry', '测试修复重复的模板参数');
+		return;
+	}
+	if (!module.parent) {
+		await api[mode === 'dry' ? 'login' : 'csrfToken']();
+		if (mode === 'rerun') {
+			await api.massEdit(null, mode, '自动修复无效自封闭的HTML标签');
+			return;
+		}
+	}
+	const pages = await api.categorymembers('使用无效自封闭HTML标签的页面');
 	const list = pages.map(({pageid, content}) => {
 		if (/{{[\s\u200e]*(?:[Ii]nuse|施工中|[编編][辑輯]中)/.test(content)) {
 			error(`已跳过施工中的页面 ${pageid} ！`);
@@ -37,13 +51,7 @@ const main = async (api = new Api(user, pin, url)) => {
 			error(`页面 ${pageid} 未找到无效自封闭的HTML标签！`);
 			return null;
 		}
-		const text = content.replace(regex, (p0, p1) => {
-			if (p0.slice(p1.length + 1, -2)) { // 这大概率是一个独立标签
-				return `${p0.slice(0, -2)}></${p1}>`;
-			}
-			// 这大概率是一个错误的闭合标签
-			return `</${p1}>`;
-		});
+		const text = _analyze(content, regex);
 		return [pageid, content, text];
 	}).filter(page => page);
 	await api.massEdit(list, mode, '自动修复无效自封闭的HTML标签');
