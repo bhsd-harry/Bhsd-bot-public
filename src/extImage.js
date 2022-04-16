7affeaa3736e3a3d4858f370f7b8468d4a614860
bhsd <2545473905@qq.com>
2022-04-16 00:41:54 -0700

    fix: extImage.js

--- src/extImage.js
+++ src/extImage.js
@@ -6,23 +6,25 @@ const Api = require('../lib/api.js'),
 	{user, pin, url} = require('../config/user.json'),
 	{error, runMode} = require('../lib/dev.js');
 
-const api = new Api(user, pin, url),
-	regex = /https:\/\/(?:i\d\.hdslb\.com|w[wx]\d\.sinaimg\.cn)\/[\S]+\.(?:jpe?g|png|gif|tiff|bmp)/gi;
+const regex = /https:\/\/(?:i\d\.hdslb\.com|w[wx]\d\.sinaimg\.cn)\/[^\s[\]<>"|{}]+\.(?:jpe?g|png|gif|tiff|bmp)/gi;
 
-const search = site => api.search(`insource:"https://${site}" -hastemplate:"NoReferer"`);
-
-(async () => {
+const main = async (api = new Api(user, pin, url)) => {
 	const mode = runMode();
-	await api[mode === 'dry' ? 'login' : 'csrfToken']();
-	if (mode === 'rerun') {
-		await api.massEdit(null, mode, '自动修复引自bilibili或新浪的图片外链');
-		return;
+	if (!module.parent) {
+		await api[mode === 'dry' ? 'login' : 'csrfToken']();
+		if (mode === 'rerun') {
+			await api.massEdit(null, mode, '自动修复引自bilibili或新浪的图片外链');
+			return;
+		}
 	}
+
+	const _search = site => api.search(`insource:"https://${site}"`);
+
 	// i[0-2].hdslb.com或ww[1-4].sinaimg.cn
 	const pages = (await Promise.all([
-		...new Array(3).fill().map((_, i) => search(`i${i}.hdslb.com`)),
-		...new Array(4).fill().map((_, i) => search(`ww${i + 1}.sinaimg.cn`)),
-		...new Array(4).fill().map((_, i) => search(`wx${i + 1}.sinaimg.cn`)),
+		...new Array(3).fill().map((_, i) => _search(`i${i}.hdslb.com`)),
+		...new Array(4).fill().map((_, i) => _search(`ww${i + 1}.sinaimg.cn`)),
+		...new Array(4).fill().map((_, i) => _search(`wx${i + 1}.sinaimg.cn`)),
 	])).flat();
 	const pageids = [...new Set(pages.map(({pageid}) => pageid))],
 		edits = pageids.map(pageid => pages.find(({pageid: id}) => id === pageid)).map(({pageid, content}) => {
@@ -33,9 +35,15 @@ const search = site => api.search(`insource:"https://${site}" -hastemplate:"NoRe
 			}
 			let text = content;
 			urls.forEach(imgUrl => {
-				text = text.replace(url, `http://${imgUrl.slice(8)}`);
+				text = text.replace(imgUrl, `http://${imgUrl.slice(8)}`);
 			});
 			return [pageid, content, text];
 		}).filter(edit => edit);
 	await api.massEdit(edits, mode, '自动修复引自bilibili或新浪的图片外链');
-})();
+};
+
+if (!module.parent) {
+	main();
+}
+
+module.exports = main;
--- src/repeated-and-tag.js
+++ src/repeated-and-tag.js
@@ -3,7 +3,8 @@ const {user, pin, url} = require('../config/user.json'),
 	Api = require('../lib/api.js'),
 	{runMode} = require('../lib/dev.js'),
 	repeated = require('./repeated-CeL.js'),
-	tag = require('./tag.js');
+	tag = require('./tag.js'),
+	extImage = require('./extImage.js');
 
 const api = new Api(user, pin, url);
 
@@ -24,6 +25,13 @@ const api = new Api(user, pin, url);
 		console.log('tag.js已执行！');
 	} catch (e) {
 		console.log('tag.js执行过程中发生错误！');
+		console.error(e);
+	}
+	try {
+		await extImage(api);
+		console.log('extImage.js已执行！');
+	} catch (e) {
+		console.log('extImage.js执行过程中发现错误！');
 		console.error(e);
 	}
 })();
