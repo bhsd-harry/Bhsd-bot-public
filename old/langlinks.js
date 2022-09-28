b65af18da218363c42e4fb16450eb0220bd1abeb
bhsd <2545473905@qq.com>
2022-09-28 16:33:48 -0700

    fix(langlink.js): 英萌暂时不可用

--- old/langlinks.js
+++ old/langlinks.js
@@ -2,13 +2,13 @@
 'use strict';
 
 const Api = require('../lib/api'),
-	{save, runMode, error} = require('../lib/dev'),
-	{user, pin, url, enpin, japin} = require('../config/user'),
+	{save, runMode, error, info} = require('../lib/dev'),
+	{user, pin, url, enpin, enurl, japin, jaurl} = require('../config/user'),
 	Parser = require('wikiparser-node'),
 	records = require('../config/langlinks'),
 	config = {
-		ja: {url: 'https://ja.moegirl.org.cn', pin: japin, name: '日'},
-		en: {url: 'https://en.moegirl.org.cn', pin: enpin, name: '英'},
+		ja: {url: jaurl, pin: japin, name: '日'},
+		en: {url: enurl, pin: enpin, name: '英'},
 		zh: {url, pin, name: '中'},
 	},
 	sourceParams = {
@@ -20,117 +20,132 @@ const Api = require('../lib/api'),
 	},
 	newLinks = [],
 	apis = {},
-	edits = {ja: [], en: [], zh: []};
+	corrections = {ja: [], en: [], zh: []},
+	mode = runMode();
 Parser.warning = false;
 Parser.config = './config/moegirl';
 
-const ucfirst = str => str[0].toUpperCase() + str.slice(1);
-
-const getSourcePages = (q, source) => {
-	const pages = q.query.pages.map(({title, langlinks}) => ({
-		title,
-		langlinks: langlinks?.map(({lang, title: target}) => ({lang, title: ucfirst(target)}))
-			?.filter(({title: target}) => !target.startsWith('Template:')),
-	})).filter(({langlinks}) => langlinks?.length);
+const getLangLinks = async (source, c = {}) => {
+	const sourceApi = apis[source],
+		q = await sourceApi.get({...sourceParams, ...c, lllang: source === 'ja' ? 'zh' : 'ja'}),
+		pages = q.query.pages.map(({title, langlinks}) => ({
+			title,
+			langlinks: langlinks
+				?.map(({lang, title: target}) => ({lang, title: Parser.normalizeTitle(target).title}))
+				?.filter(({title: target}) => !target.startsWith('Template:')),
+		})).filter(({langlinks}) => langlinks?.length);
 	for (const {title, langlinks} of pages) {
-		const page = records.find(record => record[source] === title);
-		if (!page) {
-			newLinks.push({source, title, langlinks});
+		const record = records.find(r => r[source] === title);
+		if (!record) {
+			newLinks.push({source, title, links: langlinks});
 			continue;
 		}
-		const newLangLinks = langlinks.filter(({lang, title: target}) => page[lang] !== target);
+		const newLangLinks = langlinks.filter(({lang, title: target}) => record[lang] !== target);
 		if (newLangLinks.length) {
-			newLinks.push({source, title, langlinks: newLangLinks});
+			newLinks.push({source, title, links: newLangLinks});
 		}
 	}
-};
-
-const getLangLinks = async (sourceApi, source, c = {}) => {
-	const q = await sourceApi.get({...sourceParams, ...c});
-	getSourcePages(q, source);
 	if (q.continue) {
-		return getLangLinks(sourceApi, source, q.continue);
+		return getLangLinks(source, q.continue);
 	}
 };
 
 const sourceMain = async source => {
 	const sourceApi = new Api(user, config[source].pin, config[source].url);
-	await sourceApi.csrfToken();
-	await getLangLinks(sourceApi, source);
 	apis[source] = sourceApi;
+	await sourceApi.login();
+	await getLangLinks(source);
 };
 
 const targetMain = async target => {
 	const targetApi = apis[target],
 		{name} = config[target],
 		titles = [...new Set(
-			newLinks.map(({langlinks}) => langlinks.find(({lang}) => lang === target)?.title).filter(title => title),
+			newLinks.map(({links}) => links.find(({lang}) => lang === target)?.title).filter(title => title),
 		)];
-	if (titles.length) {
-		const q = await targetApi.post({titles, ...targetParams}),
-			conversion = [...q.query.normalized ?? [], ...q.query.converted ?? [], ...q.query.redirects ?? []];
-		for (const {from, to} of conversion) {
-			const convertedLangLinks = newLinks
-				.filter(({langlinks}) => langlinks.some(({lang, title}) => lang === target && title === from));
-			for (const {source, title} of convertedLangLinks) {
-				edits[source].push({title, target, from, to});
-			}
+	if (!titles.length) {
+		return;
+	}
+	const {query} = await targetApi.post({titles, ...targetParams, lllang: target === 'ja' ? 'zh' : 'ja'}),
+		conversion = [...query.converted ?? [], ...query.redirects ?? []];
+	for (const {from, to} of conversion) {
+		const converted = newLinks
+			.filter(({links}) => links.some(({lang, title}) => lang === target && title === from));
+		for (const {source, title} of converted) {
+			corrections[source].push({
+				title, target, from,
+				to: conversion.find(({from: secondFrom}) => secondFrom === to)?.to ?? to,
+			});
 		}
-		const pages = q.query.pages.filter(({missing}) => !missing);
-		for (const {pageid, title, langlinks} of pages) {
-			let appendtext = '';
-			const relevantLinks = newLinks.filter(({langlinks: links}) =>
-				links.some(({lang, title: langlink}) => lang === target && ucfirst(langlink) === title),
-			);
-			for (const {source, title: langlink} of relevantLinks) {
-				const sourceLink = langlinks?.find(({lang}) => lang === source)?.title;
-				if (!sourceLink) {
-					appendtext += `[[${source}:${langlink}]]`;
-					const record = records.find(pair => pair[target] === title || pair[source] === langlink);
-					if (record) {
-						record[target] = title;
-						record[source] = langlink;
-					} else {
-						records.push({[target]: title, [source]: langlink});
-					}
-				} else if (ucfirst(sourceLink) !== langlink) {
-					error(`${name}萌页面 ${pageid} 的${
-						config[source].name
-					}萌链接 [[${sourceLink}]] 与 [[${langlink}]] 不匹配！`);
+	}
+	const pages = query.pages.filter(({missing}) => !missing);
+	for (const {pageid, title, langlinks} of pages) {
+		let appendtext = '';
+		const relevantLinks = newLinks.filter(({links}) =>
+			links.some(({lang, title: link}) => lang === target && Parser.normalizeTitle(link).title === title),
+		);
+		for (const {source, title: sourceTitle} of relevantLinks) {
+			const sourceLink = langlinks?.find(({lang}) => lang === source)?.title;
+			if (!sourceLink) {
+				appendtext += `[[${source}:${sourceTitle}]]`;
+				const record = records.find(r => r[target] === title || r[source] === sourceTitle);
+				if (record) {
+					record[target] = title;
+					record[source] = sourceTitle;
+				} else {
+					records.push({[target]: title, [source]: sourceTitle});
 				}
-			}
-			if (appendtext) {
-				await targetApi.edit({pageid, appendtext, summary: `自动添加跨语言链接（测试）`});
+			} else if (Parser.normalizeTitle(sourceLink).title !== sourceTitle) {
+				const sourceName = config[source].name;
+				error(`${name}萌页面 ${pageid} 的${sourceName}萌链接 [[${
+					sourceLink
+				}]] 与${sourceName}萌页面 [[${sourceTitle}]] 不匹配！预计将于第三步修正。`);
 			}
 		}
-		await save('../config/langlinks.json', records);
+		if (appendtext) {
+			if (mode !== 'dry') {
+				await targetApi.csrfToken();
+			}
+			await targetApi.edit({pageid, appendtext, summary: `自动添加跨语言链接`}, mode === 'dry');
+		}
 	}
 };
 
-const edit = async wiki => {
+const editMain = async wiki => {
 	const api = apis[wiki],
-		mode = runMode(),
-		pages = await api.revisions({titles: edits[wiki].map(({title}) => title)}),
+		pages = await api.revisions({titles: corrections[wiki].map(({title}) => title)}),
 		list = [];
 	for (const {pageid, title, content, timestamp, curtimestamp} of pages) {
-		const root = Parser.parse(content, false, 6),
-			{target, to} = edits[wiki].find(({title: thisTitle}) => title === thisTitle);
-		root.querySelector(`link[interwiki=${target}]`)?.setLangLink(target, to);
+		const root = Parser.parse(content, false, 6);
+		let record = records.find(r => r[wiki] === title);
+		if (!record) {
+			record = {[wiki]: title};
+		}
+		for (const {target, to} of corrections[wiki].filter(({title: thatTitle}) => title === thatTitle)) {
+			root.querySelector(`link[interwiki=${target}]`)?.setLangLink(target, to);
+			record[target] = to;
+		}
 		list.push([pageid, content, root.toString(), timestamp, curtimestamp]);
 	}
-	await api.massEdit(list, mode, '修正跨语言链接（测试）');
+	if (mode !== 'dry') {
+		await api.csrfToken();
+	}
+	await api.massEdit(list, mode, '修正跨语言链接');
 };
 
 const main = async () => {
-	for (const source of ['ja', 'en', 'zh']) {
+	for (const source of ['ja', 'zh']) {
 		await sourceMain(source);
 	}
-	for (const target of ['ja', 'en', 'zh']) {
+	info('第一步：检查所有跨语言链接页面执行完毕。');
+	for (const target of ['ja', 'zh']) {
 		await targetMain(target);
 	}
-	for (const wiki of ['ja', 'en', 'zh']) {
-		await edit(wiki);
+	info('第二步：检查待修改的跨语言链接页面执行完毕。');
+	for (const wiki of ['ja', 'zh']) {
+		await editMain(wiki);
 	}
+	await save('../config/langlinks.json', records);
 };
 
 main();
