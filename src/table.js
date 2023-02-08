dbccd8b46dc8ff676f45e7af6aba9ab3c4caacc4
bhsd <2545473905@qq.com>
2023-02-07 22:17:19 -0800

    feat(table.js): 自动修复不正确的表格闭合语法

--- src/table.js
+++ src/table.js
@@ -15,23 +15,25 @@ Parser.config = './config/moegirl';
 		await api[mode === 'dry' ? 'login' : 'csrfToken']();
 	}
 	if (mode === 'rerun' || mode === 'redry') {
-		await api.massEdit(null, mode, '自动修复语法错误的HTML标签');
+		await api.massEdit(null, mode, '自动修复未闭合的表格');
 		return;
 	}
 	const targets = Object.entries(lintErrors).filter(([, {errors}]) => errors.some(
-			({message, excerpt}) => message === '包含无效属性' && excerpt?.trim() === '/'
-				|| message === '同时闭合和自封闭的标签' && /^<\/br\s*>$/iu.test(excerpt),
+			({message, excerpt}) => message === '未闭合的表格' && excerpt?.startsWith('\n|-}'),
 		)),
 		edits = [],
 		pages = await api.revisions({pageids: targets.map(([pageid]) => pageid)});
 	for (const {pageid, content, timestamp, curtimestamp} of pages) {
-		const root = Parser.parse(content, false, 3);
-		for (const br of root.querySelectorAll('html#br')) {
-			br.closing = false;
-			const {firstChild} = br;
-			if (String(firstChild).trim() === '/') {
-				firstChild.sanitize();
-				br.selfClosing = true;
+		const root = Parser.parse(content, false, 4);
+		for (const table of root.querySelectorAll('table[closed=false]')) {
+			const tr = table.lastChild,
+				str = String(tr);
+			if (str.startsWith('\n|-}')) {
+				Parser.run(() => {
+					table.close('\n|}');
+				});
+				tr.remove();
+				table.after(str.slice(4));
 			}
 		}
 		const text = String(root);
@@ -39,5 +41,5 @@ Parser.config = './config/moegirl';
 			edits.push([pageid, content, text, timestamp, curtimestamp]);
 		}
 	}
-	await api.massEdit(edits, mode, '自动修复语法错误的HTML标签');
+	await api.massEdit(edits, mode, '自动修复未闭合的表格');
 })();
